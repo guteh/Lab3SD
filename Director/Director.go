dde34171455739f99908c8ffbbe6685947cd839c
Guteh <benjamin.gutierrezc@usm.cl>
2024-05-17 00:18:05 -0400

    sis

--- Director/Director.go
+++ Director/Director.go
@@ -1,1 +1,68 @@
+package main
 
+import (
+	pb "Lab3SD/Proto"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+var mutex = &sync.Mutex{} //Se crea un mutex para evitar problemas de concurrencia
+
+
+
+type server struct {  //Crea el servidor rcp
+    pb.UnimplementedMercDirServer
+	fase int
+	grpcServer *grpc.Server
+}
+
+//Implementa la funcion SolicitarM de la interfaz ServicioRecursos de RCP
+
+func (s *server) MensajeDirector(ctx context.Context, req *pb.MercenarioMensaje) (*pb.DirectorMensaje, error) {
+	mutex.Lock() //Bloquea recursos
+
+
+    if req.GetPeticion() == 1 { 
+        print("ostia chaval")
+        return &pb.DirectorMensaje{Inicio: 1}, nil //Se retorna en funcion SolicitarM un mensaje de aprobacion
+
+    } else {
+        // No hay suficientes recursos
+
+        return &pb.DirectorMensaje{Inicio: 0}, nil  //Se retorna en funcion SolicitarM un mensaje de denegacion
+    }
+}
+
+
+
+
+func main() {
+
+	grpcServer := grpc.NewServer() //Se crea el servidor
+
+	mutex.Lock()
+	s := &server{ //Se le asignan los recursos al servidor
+		fase: 0,
+		grpcServer: grpcServer,
+    }
+	mutex.Unlock()
+
+	pb.RegisterMercDirServer(grpcServer, s) //Se registra el servidor
+	
+	addr := "10.35.169.91:8080"  //Se asigna la direccion del servidor
+	lis, err := net.Listen("tcp", addr) //Se crea el listener
+    if err != nil {
+		log.Fatalf("Fallo al escuchar %v", err)
+    }
+	
+	if err := grpcServer.Serve(lis); err != nil {  //Se inicia el servidor
+        log.Fatalf("Fallo al crear servidor: %s", err)
+    }
+	
+	fmt.Printf("Director jeje\n")
+
+}
--- Mercenarios/Mercenarios.go
+++ Mercenarios/Mercenarios.go
@@ -1,1 +1,59 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	pb "Lab3SD/Proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+
+
+	// Se crean los cuatro grupos
+	for i := 0; i < 1; i++ {  // Se crean 4 grupos, si se quiere modificar se debe cambiar el 4 por otro numero y el archivo Central.go linea 53
+		wg.Add(1)
+		go InicioMercenario(i + 1, &wg)  //Empieza ejecucion de equipo
+		fmt.Printf("Mercenario %d redi!\n", i+1)
+	}
+
+	// Espera que todas las ejecuciones terminen para finalizar la ejecucion del codigo.
+	wg.Wait()
+
+}
+
+func InicioMercenario(id int,wg *sync.WaitGroup) { //Toma como parametros el id del equipo y el grupo de espera
+
+	defer wg.Done()
+
+	// Genera cantidades random de recursos
+
+	serverAddr := "10.35.169.91:8080"
+	conn, err := grpc.NewClient(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))  //Se conecta al servidor central
+	if err != nil {
+		fmt.Println("Error al conectar al servidor central:", err)
+		return
+	}
+	defer conn.Close()
+
+	c := pb.NewMercDirClient(conn)
+
+	for {
+		response, err := c.MensajeDirector(context.Background(), &pb.MercenarioMensaje{Peticion: 1, Decision: 1})  //Envia peticion de recursos a servidor central
+		if err != nil {
+			fmt.Println("Error al enviar el mensaje al servidor central:", err)
+		}
+		if response.Inicio == 1 {
+			fmt.Printf("Se inicia mision")
+			break  //Si la respuesta es 1, se cierra la comunicacion
+		} else {
+			fmt.Printf("No hay mision")
+		}
+	}
+}
